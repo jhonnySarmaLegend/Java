5c805bbc3272aa77fa4a4f1377bf4096ff949304
jhonnySarmaLegend <jhonny.sarma.legend@gmail.com>
2025-08-26 21:38:08 +0530

    Create Jumper.java

---
+++ 8. Machine Coding Round/1. Snake & Ladder/src/SnL/Model/Jumper.java
@@ -0,0 +1,11 @@
+package SnL.Model;
+
+public class Jumper {  
+    public final int startPoint;  
+    public final int endPoint;  
+
+    public Jumper(int startPoint, int endPoint) {  
+        this.startPoint = startPoint;  
+        this.endPoint = endPoint;  
+    }  
+} 
